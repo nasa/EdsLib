2dcf5dd4e22826b9edbb1cb7bfaead643d353818
Joseph Hickey <joseph.p.hickey@nasa.gov>
2023-03-13 13:00:10 -0400

    Fix #39, scrub line ending whitespace
    Remove extra whitespace from all line endings

--- edslib/fsw/src/edslib_displaydb_api.c
+++ edslib/fsw/src/edslib_displaydb_api.c
@@ -1,17 +1,17 @@
 /*
  * LEW-19710-1, CCSDS SOIS Electronic Data Sheet Implementation
  * LEW-20211-1, Python Bindings for the Core Flight Executive Mission Library
- * 
+ *
  * Copyright (c) 2020 United States Government as represented by
  * the Administrator of the National Aeronautics and Space Administration.
  * All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
